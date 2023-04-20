97e010338957ca48ecc1dd775187a54585b70df1
thiripwint645 <thiripwint21@gmail.com>
2023-04-20 22:55:22 +0630

    first_commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./src/**/*.{html,js,css}"],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
